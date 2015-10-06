93d917fd4929737ccfc70fa4252329e4152c32dc
Mark Buchthal <mark.buchthal@gmail.com>
2015-10-06 10:44:50 -0700

    correct gulpfile and indexhtml

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,12 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 
-
 gulp.task("sass", function() {
- gulp.src("sass/**/*.scss")
- .pipe(sass().on("error", sass.logError))
- .pipe(gulp.dest("public/css"));
+  gulp.src("public/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./public/css"));
 });
 
-gulp.task("default", function() {
- gulp.watch("sass/**/*.scss", ["sass"]);
+gulp.task("watch", function() {
+  gulp.watch("public/sass/**/*.scss", ["sass"]);
 });
