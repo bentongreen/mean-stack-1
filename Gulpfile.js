156939a17d70fffe6f14c10abf5e1a71cca66577
Mark Buchthal <mark.buchthal@gmail.com>
2015-10-07 11:41:13 -0700

    correct gulpfile with webpack stream, restructure js directory

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var webpack = require("webpack");
+var stream = require("webpack-stream");
 
 gulp.task("sass", function() {
   gulp.src("public/sass/**/*.scss")
@@ -7,30 +9,17 @@ gulp.task("sass", function() {
     .pipe(gulp.dest("./public/css"));
 });
 
-gulp.task("watch", function() {
-  gulp.watch("public/sass/**/*.scss", ["sass"]);
+gulp.task("stream", function(callback) {
+  return gulp.src('js/**/*.js')
+  .pipe(stream({
+    output: {
+      filename: 'packed.js'
+    }
+  }))
+  .pipe(gulp.dest('./js'));
 });
 
-// var gulp = require("gulp");
-// var sass = require("gulp-sass");
-// var autoprefixer = require("autoprefixer");
-// var sourcemaps = require("gulp-sourcemaps");
-// var input = "public/sass/**/*.scss";
-// var output = "./public/css";
-// var autoprefixerOptions = {
-//   browsers: ['last 2 versions']
-// };
-
-// gulp.task("sass", function() {
-//   return gulp
-//     .src(input)
-//     .pipe(sourcemaps.init())
-//     .pipe(sass().on("error", sass.logError))
-//     .pipe(sourcemaps.write("./stylesheets/maps"))
-//     .pipe(autoprefixer(autoprefixerOptions))
-//     .pipe(gulp.dest(output));
-// });
-
-// gulp.task("watch", function() {
-//   gulp.watch("public/sass/**/*.scss", ["sass"]);
-// });
+gulp.task("default", function() {
+  gulp.watch("public/sass/**/*.scss", ["sass"]);
+  gulp.watch("js/**/*.js", ["stream"]);
+});
