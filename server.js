de0d94f41b667ab37bfbeef5df2e7c9c2b6eb42f
Mark Buchthal <mark.buchthal@gmail.com>
2015-10-07 10:49:24 -0700

    fix serverjs file to load files

--- server.js
+++ server.js
@@ -4,26 +4,30 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-var server = app.listen(port, 'localhost', function() {
-  var port = server.address().port;
-  var host = server.address().address;
-  console.log('server listening at http:// ' + host + ':' + port);
-});
+// var server = app.listen(port, 'localhost', function() {
+//   var port = server.address().port;
+//   var host = server.address().address;
+//   console.log('server listening at http://' + host + ':' + port);
+// });
 
-app.route('*')
-  .get(function (req, res, next) {
-    console.log('request was made...')
-});
-
-app.route('/')
-  .get(function(req, res) {
-    res.sendFile('index.html', { root: __dirname + '/' });
-});
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(function(req, res) {
-    res.status(404).sendFile('404.html', { root: __dirname + '/' });
+app.get('/', function (req, res, next) {
+    console.log('request was made...');
+    console.log(req.originalUrl);
+    next();
 });
 
-app.listen();
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: __dirname + '/' });
+});
+
+app.use(function(req, res) {
+  console.log(req.originalUrl);
+    res.status(404).sendFile('404.html', { root: __dirname + '/' });
+
+});
+
+app.listen(port);
+console.log('server listening at' + port);
