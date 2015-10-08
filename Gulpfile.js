f46b82af7c7a545a53e20d6d2b5b769cb70ff450
Mark Buchthal <mark.buchthal@gmail.com>
2015-10-08 08:43:06 -0700

    correct file paths in gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,14 +5,14 @@ var stream = require("webpack-stream");
 var livereload = require("gulp-livereload");
 
 gulp.task("sass", function() {
-  gulp.src("public/sass/**/*.scss")
+  gulp.src("src/sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./public/css"))
+    .pipe(gulp.dest("public/css"))
     .pipe(livereload());
 });
 
 gulp.task("stream", function(callback) {
-  return gulp.src('js/**/*.js')
+  return gulp.src('src/js/**/*.js')
   .pipe(stream({
     output: {
       filename: 'packed.js'
@@ -23,6 +23,6 @@ gulp.task("stream", function(callback) {
 
 gulp.task("default", function() {
   livereload.listen();
-  gulp.watch("public/sass/**/*.scss", ["sass"]);
-  gulp.watch("js/**/*.js", ["stream"]);
+  gulp.watch("src/sass/**/*.scss", ["sass"]);
+  gulp.watch("src/js/**/*.js", ["stream"]);
 });
