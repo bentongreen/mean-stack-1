5eeba916f2c94e14234d808f7f319dbae1285b72
Mark Buchthal <mark.buchthal@gmail.com>
2015-10-09 10:01:18 -0700

    update gulp files and packages

--- gulp/tasks/jshint.js
+++ gulp/tasks/jshint.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var jshint = require("gulp-jshint");
 
 gulp.task("jshint", function() {
-  return gulp.src("src/app/**/*.js"))
+  return gulp.src("src/app/**/*.js")
   .pipe(jshint())
   .pipe(jshint.reporter("jshint-stylish"));
 });
