2b2a371d26574251e6f59c22812451e1a3f98b51
Jed Foster <jed@jedfoster.com>
2015-10-12 16:28:33 -0700

    Initial implementation of the Github API

--- src/app/blogs/blog.ctrl.js
+++ src/app/blogs/blog.ctrl.js
@@ -10,7 +10,16 @@ angular.module("blogapp").controller("BlogCtrl", ["BlogsService", "$routeParams"
     BlogsService
       .get($routeParams.blog_id)
       .then(function (resp) {
-        vm.blog = resp.data;
+        console.log(resp);
+
+        vm.blog = {};
+        vm.blog.content = '';
+        for(file in resp.files) {
+          vm.blog.content += resp.files[file].content;
+        }
+
+        vm.blog.date = resp.updated_at;
+        vm.blog.author = resp.owner.login;
       });
   }
 }]);
