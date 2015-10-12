8fcd0bfa1e26697cb0e68318761d8e062f0d731c
Mark Buchthal <mark.buchthal@gmail.com>
2015-10-12 10:05:00 -0700

    remove extra files, standardize quotations in gulpfiles

--- gulp/tasks/jshint.js
+++
@@ -1,8 +0,0 @@
-var gulp = require("gulp");
-var jshint = require("gulp-jshint");
-
-gulp.task("jshint", function() {
-  return gulp.src("src/app/**/*.js")
-  .pipe(jshint())
-  .pipe(jshint.reporter("jshint-stylish"));
-});
--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,11 +1,11 @@
 
-var gulp = require('gulp');
-var config = require('../config').server;
-var server = require('gulp-express');
+var gulp = require("gulp");
+var config = require("../config").server;
+var server = require("gulp-express");
 
-gulp.task('server', function() {
-  server.run(['server.js']);
+gulp.task("server", function() {
+  server.run(["server.js"]);
 
   // restart the server when it changes
-  gulp.watch(['server.js'], [server.run]);
+  gulp.watch(["server.js"], [server.run]);
 });
